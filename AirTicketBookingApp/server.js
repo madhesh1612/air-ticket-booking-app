94ddeaf94918b99838f25b84a2f65207472e83a6
Muthu Madhesh <muthumadesh428@gmail.com>
2025-08-08 09:27:56 +0530

    Update server.js

--- AirTicketBookingApp/server.js
+++ AirTicketBookingApp/server.js
@@ -89,7 +89,7 @@ app.post('/book', (req, res) => {
   });
 });
 
-// Start server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`✅ Server is running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
